dd65bfdc8b87a2cb1ad7d2d45e9fb9e2a51b0ed1
Akash Kumar <91385321+OfficialAkashKumar@users.noreply.github.com>
2022-04-08 16:05:19 +0530

    Create merge sort.cpp

---
+++ Recursion/merge sort.cpp
@@ -0,0 +1,47 @@
+/*
+  https://www.topcoder.com/thrive/articles/Sorting
+
+    https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/tutorial/
+
+
+*/
+
+void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+         int n1 = m-l+1, n2 = r-m;
+         int left[n1], right[n2];
+         for(int i = 0; i < n1; i++)
+            left[i] = arr[l+i];
+         for(int i = 0; i < n2; i++)
+            right[i] = arr[m+i+1];
+        
+        int i = 0, j = 0, k = l;
+        while(i < n1 && j < n2)
+        {
+            if(left[i] <= right[j])
+                arr[k++] = left[i++];
+            else
+                arr[k++] = right[j++];
+        }
+        while(i < n1)
+        {
+            arr[k++] = left[i++];
+        }
+        while(j < n2)
+        {
+            arr[k++] = right[j++];
+        }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if(l >= r)
+            return;
+            
+            int m = (l+r)/2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+            merge(arr, l, m, r);
+    }
