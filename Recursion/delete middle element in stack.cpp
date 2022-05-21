c29dc5034d81ccded80834e03e719afb39e9bfb4
Akash Kumar <91385321+OfficialAkashKumar@users.noreply.github.com>
2022-05-21 18:01:40 +0530

    Update delete middle element in stack.cpp

--- Recursion/delete middle element in stack.cpp
+++ Recursion/delete middle element in stack.cpp
@@ -1,59 +1,48 @@
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
-using namespace placeholders;
 
-#define ll long long
-
-#define MOD 1000000009
-
-void solve(stack<int> &s, int k) {
-	if (k == 1) {
-		s.pop();
+void middel(stack<int> &st, int n) {
+	if (n == 1) {
+		st.pop();
 		return;
 	}
-	int temp = s.top();
-	s.pop();
-	solve(s, k - 1);
-	s.push(temp);
+	int temp = st.top();
+	st.pop();
+	middel(st, n - 1);
+	st.push(temp);
+	return;
 }
 
-
-// mid = k = (st.size() + 1) / 2;
-
-//    5   
-//    4    
-//    1 --> mid = (5 + 1) / 2 = 3;
-//    2
-//    3
-
-stack<int> middel(stack<int> &s) {
-	if (s.size() == 0)
-		return s;
-	int k = s.size() / 2 + 1;
-	solve(s, k);
-	return s;
+void print(stack<int> st) {
+	if (st.size() == 0) {
+		return;
+	}
+	int temp = st.top();
+	st.pop();
+	print(st);
+	cout << temp << " ";
+	st.push(temp);
 }
-
 
 int main() {
 	std::ios::sync_with_stdio(false);
 	std::cin.tie(nullptr);
+
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-	stack<int> v;
 	int n; cin >> n;
+	int elem;
+	stack<int> st;
 	for (int i = 0; i < n; ++i) {
-		int num; cin >> num;
-		v.push(num);
+		cin >> elem;
+		st.push(elem);
 	}
-	middel(v);
-	while (!v.empty()) {
-		cout << v.top() << endl;
-		v.pop();
-	}
+	middel(st, st.size() / 2 + 1);
+	print(st);
 }
+
+
+
+
