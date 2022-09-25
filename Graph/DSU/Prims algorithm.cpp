34748059ac7253be67a42046315ca3b499480e0b
Akash Kumar <91385321+OfficialAkashKumar@users.noreply.github.com>
2022-09-25 22:37:13 +0530

    Create Prims algorithm.cpp

---
+++ Graph/DSU/Prims algorithm.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace placeholders;
+#define mem1(a) 		memset(a, 1, sizeof(a))
+#define mem0(a)			memset(a, 0, sizeof(a))
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define fr first
+#define sc second
+#define ll long long
+#define prinst(st)		for(auto &i : st) cout << i << " "; cout << endl;
+#define printvec(vec) for(auto elem : vec) cout << elem << " "; cout << endl;
+#define printstack(st) while(!st.empty()) cout << st.top() << " ", st.pop(), cout <<endl;
+
+int main() {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n, m;
+	cin >> n >> m;
+	vector<pair<int, int>> gr[n + 1];
+	int vis[n + 1] = {};
+	for (int i = 0; i < m; ++i) {
+		int x, y, w;
+		cin >> x >> y >> w;
+		gr[x].push_back({y, w});
+		gr[y].push_back({x, w});
+	}
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> Q;
+	Q.push({0, 1});
+	ll mst = 0;
+	while (!Q.empty()) {
+		pair<int, int> best = Q.top();
+		Q.pop();
+
+		int weight = best.first;
+		int to = best.second;
+
+		if (vis[to]) continue;
+
+		vis[to] = 1;
+		mst += weight;
+
+		for (auto x : gr[to]) {
+			if (vis[x.first] == 0) {
+				Q.push({x.second, x.first});
+			}
+		}
+	}
+	cout << mst << endl;
+}
